2679a5d0b8b3ad314e7d8e277528a1d528bc201d
Jin Woo Han <jin00woo@naver.com>
2024-02-27 19:59:27 +0900

    Created store.

--- parsnip/src/App.js
+++ parsnip/src/App.js
@@ -2,21 +2,6 @@ import React from "react";
 import TasksPage from "./components/TasksPage";
 import "./App.css";
 
-const mockTasks = [
-  {
-    id: "1",
-    title: "Learn Redux",
-    description: "The store, actions, and reducers, oh my!",
-    status: "In Progress",
-  },
-  {
-    id: "2",
-    title: "Peace on Earth",
-    description: "No big deal.",
-    status: "In Progress",
-  },
-];
-
 const App = () => {
   return (
     <div className="main-content">
--- parsnip/src/index.js
+++ parsnip/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+import tasks from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(tasks);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
