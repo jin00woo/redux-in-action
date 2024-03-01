ad6da697dd9efdab2389480ea4a41e099579bf96
Jin Woo Han <jin00woo@naver.com>
2024-03-01 23:12:29 +0900

    Added time option

--- dave/board/src/features/posts/postsSlice.js
+++ dave/board/src/features/posts/postsSlice.js
@@ -1,8 +1,22 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
-  { id: "1", title: "First Post!", content: "Hello!" },
-  { id: "2", title: "Second Post", content: "More text" },
+  {
+    id: "1",
+    title: "First Post!",
+    content: "Hello!",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+  },
+
+  {
+    id: "2",
+    title: "Second Post",
+    content: "More text",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+  },
 ];
 
 const postsSlice = createSlice({
@@ -20,6 +34,7 @@ const postsSlice = createSlice({
             title,
             content,
             userId,
+            date: new Date().toISOString(),
           },
         };
       },
