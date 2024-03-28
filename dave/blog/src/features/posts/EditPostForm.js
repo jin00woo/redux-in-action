ebff97a1573fbb41a95da9bbde334964ee568d3e
Jin Woo Han <jin00woo@naver.com>
2024-03-28 17:40:45 +0900

    Added CRUD logic.

--- dave/blog/src/features/posts/EditPostForm.js
+++ dave/blog/src/features/posts/EditPostForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectPostById, updatePost } from "./postsSlice";
-import { useNavigate, useParams } from "react-router";
+import { selectPostById, updatePost, deletePost } from "./postsSlice";
+import { useNavigate, useParams } from "react-router-dom";
 import { selectAllUsers } from "../users/usersSlice";
 
 const EditPostForm = () => {
@@ -64,6 +64,21 @@ const EditPostForm = () => {
     </option>
   ));
 
+  const onDeletePostClicked = () => {
+    try {
+      setRequestStatus("pending");
+      dispatch(deletePost({ id: post.i })).unwrap();
+      setTitle("");
+      setContent("");
+      setUserId("");
+      navigate("/");
+    } catch (err) {
+      console.error("Failed to delete the post: ", err);
+    } finally {
+      setRequestStatus("idle");
+    }
+  };
+
   return (
     <section>
       <h2>Edit Post</h2>
@@ -95,6 +110,9 @@ const EditPostForm = () => {
 
         <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
+        </button>
+        <button type="button" onClick={onDeletePostClicked}>
+          Delete Post
         </button>
       </form>
     </section>
--- dave/blog/src/features/posts/postsSlice.js
+++ dave/blog/src/features/posts/postsSlice.js
@@ -31,6 +31,22 @@ export const updatePost = createAsyncThunk(
       const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
       return response.data;
     } catch (error) {
+      return initialPost;
+    }
+  }
+);
+
+export const deletePost = createAsyncThunk(
+  "posts/deletePost",
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.delete(`${POSTS_URL}/${id}`);
+      if (response?.status === 200) {
+        return initialPost;
+      }
+      return `${response.status}: ${response.statusText}`;
+    } catch (error) {
       return error.message;
     }
   }
@@ -134,6 +150,16 @@ const postsSlice = createSlice({
         action.payload.date = new Date().toISOString();
         const posts = state.posts.filter((post) => post.id !== id);
         state.posts = [...posts, action.payload];
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Delete could not complete");
+          console.log(action.payload);
+          return;
+        }
+        const { id } = action.payload;
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = posts;
       });
   },
 });
