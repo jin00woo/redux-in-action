23a3cfe3488cc8f035c9d3c7ae9142b093bfb1c3
jin00woo <jin00woo@naver.com>
2024-05-15 21:49:40 +0900

    Applied RTK queries to deal with states.

--- dave/optimization/src/App.js
+++ dave/optimization/src/App.js
@@ -3,13 +3,14 @@ import AddPostForm from "./features/posts/AddPostForm";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import EditPostForm from "./features/posts/EditPostForm";
 import Layout from "./components/Layout";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-
         <Route index element={<PostsList />} />
 
         <Route path="post">
@@ -18,6 +19,12 @@ function App() {
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
 
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
